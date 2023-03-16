73f30e7bf2ac69674a496a17b6602ea329abcfcc
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-03-16 16:48:51 +0100

    adding redirection double/single quotes expansion

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -18,18 +18,19 @@ void	expand(t_token_lst *tokens_lst)
 			while (tokens_lst->token->args[++i])
 				expand_quotes(&(tokens_lst->token->args[i]), tokens_lst->token->type);
 		}
-		// else if(tokens_lst->token->type == AST_REDIRECTION)
-		// {
-		// 		if(tokens_lst->token->redirect_fname[0] == '$')
-		// 		{
-		// 			tmp = envp_find_node(&(tokens_lst->token->redirect_fname[1]), 0);
-		// 			if(tmp)
-		// 			{
-		// 				free(tokens_lst->token->redirect_fname);
-		// 				tokens_lst->token->redirect_fname = tmp->value;
-		// 			}
-		// 		}
-		// }
+		else if(tokens_lst->token->type == AST_REDIRECTION)
+		{
+				// if(tokens_lst->token->redirect_fname[0] == '$')
+				// {
+				// 	tmp = envp_find_node(&(tokens_lst->token->redirect_fname[1]), 0);
+				// 	if(tmp)
+				// 	{
+				// 		free(tokens_lst->token->redirect_fname);
+				// 		tokens_lst->token->redirect_fname = tmp->value;
+				// 	}
+				// }
+				expand_quotes(&(tokens_lst->token->redirect_fname), tokens_lst->token->type);
+		}
 		tokens_lst = tokens_lst->next;
 	}
 }
