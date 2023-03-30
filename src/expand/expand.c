38faa09dd22cb567540b63379aee6e75ea4feba1
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-03-30 22:07:28 +0000

    started handling ambiguous redirections

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -15,7 +15,7 @@ void	expand(t_token_lst *tokens_lst)
 		i = -1;
 		if(tokens_lst->token->type == AST_COMMAND)
 		{
-			while (tokens_lst->token->args[++i])
+			while (tokens_lst->token->args[++i] && ft_strlcmp(tokens_lst->token->args[0], "export"))
 				expand_quotes(&(tokens_lst->token->args[i]), tokens_lst->token->type);
 		}
 		else if(tokens_lst->token->type == AST_REDIRECTION)
