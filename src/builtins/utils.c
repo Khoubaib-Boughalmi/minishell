c9fb2e59c43be357587699db2ca1fd54f4b14b25
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-04-04 03:53:56 +0100

    finishing expansion

--- src/builtins/utils.c
+++ src/builtins/utils.c
@@ -7,7 +7,7 @@ int	is_builtin(char	*cmd)
 
 	i = 0;
 	
-	builtins = ft_split("echo cd export unset env pwd", ' ');
+	builtins = ft_split("cd export unset env pwd", ' ');
 	if(!builtins)
 		return (0);
 	
@@ -69,8 +69,8 @@ void handle_builtin(char **list_cmds)
 		ft_cd(list_cmds);
 	if(!ft_strlcmp(list_cmds[0], "pwd"))
 		ft_pwd();
-	if(!ft_strlcmp(list_cmds[0], "echo"))
-		ft_echo(list_cmds);
+	// if(!ft_strlcmp(list_cmds[0], "echo"))
+	// 	ft_echo(list_cmds);
 	if(!ft_strlcmp(list_cmds[0], "export"))
 		ft_export_with_args(list_cmds);
 	if(!ft_strlcmp(list_cmds[0], "unset"))
--- src/parse.c
+++ src/parse.c
@@ -1,5 +1,36 @@
 #include "../minishell.h"
 
+void 	clean_expand_args(t_token_lst *tokens_lst)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	while (tokens_lst)
+	{
+		if(tokens_lst->token->type == AST_COMMAND)
+		{
+			i = 0;
+			while(tokens_lst->token->args[i])
+			{
+				if(!ft_strlcmp(tokens_lst->token->args[i], "$"))
+				{
+					free(tokens_lst->token->args[i]);
+					j = i;
+					while (tokens_lst->token->args[j])
+					{
+						tokens_lst->token->args[j] = tokens_lst->token->args[j + 1];
+						j++;
+					}
+				}
+				i++;
+			}
+		}
+		tokens_lst = tokens_lst->next;
+	}
+	
+}
+
 int	tokenize_expand_execute(char *input)
 {
 	t_token_lst *tokens_lst;
@@ -17,6 +48,7 @@ int	tokenize_expand_execute(char *input)
 	// printf("~~~~~~~~~~~~~~~~\n");
 	// printf("~~~~~~~~~~~~~~~~\n");
 	expand(tokens_lst);
+	// clean_expand_args(tokens_lst);
 	// display_tokens(tokens_lst);
 	// executor(tokens_lst);
 	// create_lst_commands(gstruct->tokens_head);
