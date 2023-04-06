5c1ebe2ea21d2904ea00a93c3e4a0ee2dd1d88f6
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-04-06 01:46:32 +0100

    finished exit function

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -5,6 +5,23 @@ void	ft_exit(char	**list_vars)
 {
 	long long	ex_status;
 
+	int	i;
+
+	i = 0;
+	if(list_vars[1])
+	{
+		if(list_vars[1][0] == '-')
+			i++;
+		while (list_vars[1][i])
+		{
+			if(!ft_isdigit(list_vars[1][i]))
+			{
+				printf("minishell: exit: %s: numeric argument required\n", list_vars[1]);
+				exit(2);
+			}
+			i++;
+		}
+	}
 	if (list_vars_len(list_vars) > 2)
 	{
 		printf("minishell: exit: too many arguments\n");
@@ -17,6 +34,6 @@ void	ft_exit(char	**list_vars)
 		exit(gstruct->exit_status);
 	}
 	else
-		exit(0);
+		exit(gstruct->exit_status);
 	// gstruct->exit_status = 0;
 }
