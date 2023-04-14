6f19449943bce5c0961cfc005d3121a6f3be96b9
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-04-14 14:37:56 +0000

    fixing initializing envp

--- mini-ms-tester/test.sh
+++ mini-ms-tester/test.sh
@@ -44,7 +44,7 @@ exports () {
 	export single_quote_in_env='a'"'"'b'
 	export double_quote_in_env='a"b'
 	export no_whitespace="no_whitespace"
-	export equals="ab"
+	export equals="a=b"
 	export whitespace_left=" whitespace"
 	export whitespace_center="white space"
 	export whitespace_right="whitespace "
